4d300327f01bf770bc450583d8c9e45803ebb7b4
Kapil <k.d.n.pau@se15.qmul.ac.uk>
2018-12-28 22:31:46 +0000

    Updated static root

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const path = require('path');
 const ejs = require('ejs');
 const bodyParser = require('body-parser');
 app.set('view engine', 'ejs');
-app.use(express.static('static'));
+app.use(express.static(path.join(__dirname,'/static')));
 app.use( bodyParser.json() );
 app.use(bodyParser.urlencoded({
     extended: true
